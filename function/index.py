1190b2e932a531069d49bdc20bdd455416a88105
Finnian Anderson <get@finnian.io>
2017-10-10 18:35:27 +0100

    Revert AfterBurn as encountered regressions
    Signed-off-by: Finnian Anderson <get@finnian.io>

--- function/index.py
+++ function/index.py
@@ -1,10 +1,11 @@
-from http_parser.pyparser import HttpParser
 import sys
-from handler import handle
+import handler
 
-
-def read_body(content_length):
-    return sys.stdin.read(content_length)
+def get_stdin():
+    buf = ""
+    for line in sys.stdin:
+        buf = buf + line
+    return buf
 
 def read_head():
     buf = ""
@@ -14,38 +15,8 @@ def read_head():
 
         if line == "\r\n":
             break
-#        elif line == "":
-#            break #exit(1)
-
     return buf
 
-def main():
-
-    p = HttpParser()
-    while True:
-        header = read_head()
-
-        res = p.execute(header, len(header))
-        result = None
-        length_key = "content-length"
-        content_length = p.get_headers()[length_key]
-        if content_length != None:
-            body = read_body(int(content_length))
-            result = handle(body)
-        else:
-            result = handle(None)
-
-        out_buffer = "HTTP/1.1 200 OK\r\n"
-        out_buffer += "Content-Length: "+str(len(result))+"\r\n"
-        out_buffer += "\r\n"
-        out_buffer += result
-
-        sys.stdout.write(out_buffer)
-        sys.stdout.flush()
-#        sys.stdout.close()
-
-def function(input):
-    return "You said \'" + input + "\'"
-
-if __name__ == "__main__":
-    main()
+if(__name__ == "__main__"):
+    st = get_stdin()
+    handler.handle(st)
